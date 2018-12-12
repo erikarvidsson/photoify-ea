aa3fede167cf1cdd531dba1ba218470d9dc246ee
Erik Arvidsson <e@Es-MacBook-Pro.local>
2018-12-12 10:41:08 +0100

    added footer.php

---
+++ views/footer.php
@@ -0,0 +1,21 @@
+<?php
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <article>
+         <h1><?php echo $config['title']; ?></h1>
+         <p>This is the home page.</p>
+         <a href="/app/users/newaccount.php">test</a>
+         <a href="/login.php">in</a>
+         <a href="/app/users/logout.php">ut</a>
+         <a href="/app/users/newpost.php">new post</a>
+         <a href="/app/users/editprofile.php">eddit profile</a>
+     </article>
+   </body>
+ </html>
