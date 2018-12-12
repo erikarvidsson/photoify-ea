7b2d46241f557ddcf9c552d897390395b9cbd0ce
Erik Arvidsson <e@Es-MacBook-Pro.local>
2018-12-12 10:42:19 +0100

    added login.php whit login function

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php require __DIR__.'/views/header.php';  ?>
+
+
+
+<article>
+    <h1>Login</h1>
+
+    <form action="app/users/login.php" method="post">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input class="form-control" type="email" name="email" placeholder="francis@darjeeling.com" required>
+            <small class="form-text text-muted">Please provide the your email address.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="password" name="password" required>
+            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+        </div><!-- /form-group -->
+
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+</article>
+
+<?php require __DIR__.'/views/footer.php'; ?>
