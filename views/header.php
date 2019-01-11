498aab5d449d0f7be0a89bea1181b7507758d974
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-11 10:33:32 +0100

    linked stylescheets in the header

--- views/header.php
+++ views/header.php
@@ -2,3 +2,12 @@
 // Always start by loading the default application setup.
 require __DIR__.'/../app/autoload.php';
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/style/sanitize.css">
+    <link rel="stylesheet" type="text/css" href="/style/style.css">
+    <title></title>
+  </head>
+  <body>
