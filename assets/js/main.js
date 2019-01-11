33b1bf77fdb2f39a0f27dd97eda08094bf122e27
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-11 11:38:47 +0100

    added js to show img before upploding

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,14 +28,7 @@ likeForms.forEach((likeForm) => {
 });
 
 
-var sessionChecker = (req, res, next) => {
-    if (req.session.user.id) {
-        console.log('yess');
-    }else{
-      console.log('neej')
-    }}
-
-
+// sticky banner
     var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -46,3 +39,17 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+
+// show img befor uppload
+ document.getElementById("imgs").onchange = function () {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        // get loaded data and render thumbnail.
+        document.getElementById("image").src = e.target.result;
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+};
--- newpost.php
+++ newpost.php
@@ -3,8 +3,9 @@ require __DIR__.'/views/header.php';
 require __DIR__.'/views/banner.php';
 require __DIR__.'/app/posts/newpost.php';
 ?>
-<form style="padding-top: 200px;" class="newpost-form" action="newpost.php" method="post" enctype="multipart/form-data">
-  <input class="choseimg-form" type="file" name="img">
+<img id="image" />
+<form  class="newpost-form" action="newpost.php" method="post" enctype="multipart/form-data">
+  <input class="choseimg-form" type="file" name="img" id="imgs">
   <textarea name="post_text" placeholder="Write something..."></textarea>
   <button type="submit" name="button"> Post</button>
 </form>
