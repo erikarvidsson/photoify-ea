7f31f6300189510424a5b598d9fd4fc4910e9c64
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-12 13:15:17 +0100

    fixt broken link

--- views/nav.php
+++ views/nav.php
@@ -3,7 +3,7 @@
     <?php if(isset($_SESSION['user'])){ ?>
       <a class="nav-post" href="/posts.php">All post</a>
       <a class="nav-add" href="/newpost.php">new post</a>
-      <a class="nav-profile" href="/app/users/profile.php">profile</a>
+      <a class="nav-profile" href="/profile.php">profile</a>
       <!-- <a href="/app/users/logout.php">ut</a> -->
       <!-- <a href="/editprofile.php">eddit profile</a> -->
     <?php }else { ?>
