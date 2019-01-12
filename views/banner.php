cab008127d5761405b02127d939f1018a2ba789a
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-12 13:14:57 +0100

    added dropdown menue in the banner

--- views/banner.php
+++ views/banner.php
@@ -4,4 +4,13 @@
 
 <div>
   <h1 class="banner" id="navbar">Photoify</h1>
+
+
+<div class="dropdown">
+  <button onclick="myFunction()" class="dropbtn"></button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="/editprofile.php">Eddit profile</a>
+    <a href="/logout.php">Sign out</a>
+  </div >
+</div>
 </div>
