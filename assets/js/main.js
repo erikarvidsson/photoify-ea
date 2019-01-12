48c2ab64e438157a45eecd6c4f777c39cb00f60e
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-12 13:12:33 +0100

    added dropdown menue with logout and profile settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,3 +53,24 @@ window.onscroll = function() {
     // read the image file as a data URL.
     reader.readAsDataURL(this.files[0]);
 };
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
