c78b5c8c73ce7e46c4064761e9ece51bb9e41998
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-14 22:25:38 +0100

    prewiev img js

---
+++ assets/js/preview.js
@@ -0,0 +1,13 @@
+
+// show img befor uppload
+ document.getElementById("imgs").onchange = function () {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        // get loaded data and render thumbnail.
+        document.getElementById("image").src = e.target.result;
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+};
