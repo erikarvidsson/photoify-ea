72015917a86af7ff7b3ff677185520168a22f549
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-14 22:25:04 +0100

    a

--- newpost.php
+++ newpost.php
@@ -5,7 +5,7 @@ require __DIR__.'/app/posts/newpost.php';
 ?>
 <div class="newpost-container">
 <img id="image" />
-<form  class="newpost-form" action="newpost.php" method="post" placeholder="Chose image" enctype="multipart/form-data">
+<form  class="newpost-form" action="/newpost.php" method="post" placeholder="Chose image" enctype="multipart/form-data">
   <input class="nwepost-hidden" type="file" name="img" id="imgs">
   <label class="choseimg-form" for="imgs">Select file</label>
   <textarea name="post_text" placeholder="Write something..."></textarea>
@@ -14,7 +14,7 @@ require __DIR__.'/app/posts/newpost.php';
 <div>
 
 
-
+ <script src="/assets/js/preview.js"></script>
 <?php
 require __DIR__.'/views/nav.php';
 require __DIR__.'/views/footer.php';
