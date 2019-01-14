00b6984dd43d846de41060e76ab9842a11f647d2
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-14 22:27:46 +0100

    added functions for profile page

--- app/functions.php
+++ app/functions.php
@@ -16,3 +16,37 @@ if (!function_exists('redirect')) {
         exit;
     }
 }
+
+// select all imgs posted by usere
+function selectUserPost($userId, object $pdo){
+  $statement = $pdo->prepare('SELECT * FROM posts WHERE user_id = :user_id');
+  // $statement = $pdo->prepare('SELECT * FROM users');
+  if(!$statement){
+    die(var_dump($pdo->errorInfo()));
+  }
+
+  $statement->bindParam(':user_id', $userId, PDO::PARAM_STR);
+  $statement->execute();
+
+  $imgs = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+return $imgs;
+}
+
+
+
+
+function selectUserProfile($userId, object $pdo){
+  $statement = $pdo->prepare('SELECT * FROM users WHERE id = :user_id');
+  // $statement = $pdo->prepare('SELECT * FROM users');
+  if(!$statement){
+    die(var_dump($pdo->errorInfo()));
+  }
+
+  $statement->bindParam(':user_id', $userId, PDO::PARAM_STR);
+  $statement->execute();
+
+  $profileInfo = $statement->fetch(PDO::FETCH_ASSOC);
+
+return $profileInfo;
+}
