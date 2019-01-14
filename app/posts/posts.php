f9028a6bbe56acea922d0ae008f968607e28974c
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-14 22:27:03 +0100

    fixed like array and $action like/dislike

--- app/posts/posts.php
+++ app/posts/posts.php
@@ -12,7 +12,7 @@
 // $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 // get post username by user id
-$statement = $pdo->prepare('SELECT username, user_id, post_text, img, p_id FROM `users` AS u
+$statement = $pdo->prepare('SELECT username, user_id, post_text, img, p_id, post_date FROM `users` AS u
   INNER JOIN `posts` AS p ON p.user_id = u.id');
   if(!$statement){
     die(var_dump($pdo->errorInfo()));
@@ -26,6 +26,7 @@ $statement = $pdo->prepare('SELECT username, user_id, post_text, img, p_id FROM 
   foreach ($posts as $post) :
     $userId = $post['user_id'];
     $username = $post['username'];
+    $postDate = $post['post_date'];
     $folder = $userId;
     $img = $post['img'];
     $src = 'post_img/'.$img;
@@ -44,33 +45,47 @@ $statement = $pdo->prepare('SELECT username, user_id, post_text, img, p_id FROM 
 
       $likes = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-      if ($likes) {
-        $action = 'disliked';
-      } else {
-        $action = 'liked';
-      }
+
+
+      $statement = $pdo->prepare('SELECT like_user_id FROM likes WHERE post_id = :post_id AND like_user_id = :user_id');
+        if(!$statement){
+          die(var_dump($pdo->errorInfo()));
+        }
+        $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+        $statement->bindParam(':user_id', $_SESSION['user']['id'], PDO::PARAM_INT);
+        $statement->execute();
+
+
+        $liked = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+        if ($liked) {
+          $action = 'disliked';
+        } else {
+          $action = 'liked';
+        }
       ?>
 
-
-      <h4 class="postname"><?= $username ;?></h4>
-
+      <div class="post-user">
+        <h4 class="postname"><?= $username ;?></h4>
+        <h4 class="postdate"><?= $postDate ;?></h4>
+      </div>
       <!-- makes img edditble if logged in -->
-      <?php if($userId === $_SESSION['user']['id']) : ?>
+      <?php if((int)$userId === (int)$_SESSION['user']['id']) : ?>
         <a href="/editpost.php?post_id=<?= $postId; ?>">
         <?php endif ; ?>
         <img src="/app/img/<?= $src ?>" alt="lala">
-        <?php if($userId === $_SESSION['user']['id']) : ?>
+        <?php if((int)$userId === (int)$_SESSION['user']['id']) : ?>
         </a>
       <?php endif ; ?>
-
       <!-- like counter -->
       <div class="like-bar">
-        <form method="post" class="my-like-form disliked" >
+        <form data-id="<?= $postId ?>" method="post" class="my-like-form disliked" >
           <input type="hidden" name="post_id" value="<?= $postId ?>" />
           <input type="hidden" name="action" value="<?= $action ?>" />
-          <button class="like-btn" type="submit" name="action"></button>
+          <button data-id="<?= $postId ?>" class="like-btn like-btn-<?= $postId ?> <?= $action ?>" type="submit" name="action"></button>
         </form>
-        <p class="like-counter">Likes: <?php echo count($likes).' ';?></p>
+        <p class="like-counter"> <span>Likes: <?php echo count($likes).' ';?></span></p>
 
         <a class="comment-btn" href=""><img ></a>
       </div>
