973bfcae58f10b8bbedd702b44193d05a61baa73
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-15 14:27:32 +0100

    started to style eddit post

--- editpost.php
+++ editpost.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__.'/views/header.php';
+require __DIR__.'/views/banner.php';
 require __DIR__.'/app/posts/editpost.php';
 ?>
 
@@ -11,18 +12,19 @@ require __DIR__.'/app/posts/editpost.php';
     <title></title>
   </head>
   <body>
-    <img src="/app/img/post_img/<?= $post['img']; ?>" alt="lala">
+    <div class="editprofile-container">
+      <img src="/app/img/post_img/<?= $post['img']; ?>" alt="lala">
 
-    <form class="" action="editpost.php?post_id=<?= $_GET['post_id']; ?>" method="post" enctype="multipart/form-data">
-      <label for="post-text">Text:</label>
-      <input id="post-text" name="post-text" value="<?= $post['post_text']; ?>"></input>
-      <button type="submit" name="button"> uppdate text</button>
-    </form>
+      <form class="editpost-form" action="editpost.php?post_id=<?= $_GET['post_id']; ?>" method="post" enctype="multipart/form-data">
+        <label for="post-text">Description:</label>
+        <textarea id="post-text" name="post-text" value="<?= $post['post_text']; ?>"><?= $post['post_text']; ?></textarea>
+        <button type="submit" name="button"> Uppdate description</button>
+      </form>
 
-    <form class="" action="editpost.php?post_id=<?= $_GET['post_id']; ?>" method="post" enctype="multipart/form-data">
-      <button type="submit" name="delete"> delete post</button>
-    </form>
-
+      <form class="editpost-form" action="editpost.php?post_id=<?= $_GET['post_id']; ?>" method="post" enctype="multipart/form-data">
+        <button type="submit" name="delete"> Delete post</button>
+      </form>
+    <div>
   </body>
 </html>
 
