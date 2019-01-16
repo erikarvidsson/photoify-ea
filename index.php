2aa31f26888b529f3b675aeac704eb9978bbee63
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-16 19:09:50 +0100

    removed extra php open and close tags

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
 require __DIR__.'/views/header.php';
-?>
 
-
-<?php
   if(isset($_SESSION['user'])){
     redirect('/posts.php');
   }else{
     redirect('/login.php');
   }
-?>
-<?php
 
 require __DIR__.'/views/nav.php';
 require __DIR__.'/views/footer.php'; ?>
