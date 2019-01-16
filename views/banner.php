c95e05ec11c9d9a6cb8a9e237a8b1c8cf4781812
Erik Arvidsson <e@Es-MacBook-Pro.local>
2019-01-16 17:30:19 +0100

    fixed banner

--- views/banner.php
+++ views/banner.php
@@ -5,7 +5,7 @@
 <div class="banner" id="navbar">
   <h1 class="banner2">Photoify</h1>
 
-
+<?php if(isset($_SESSION['user'])) :?>
 <div class="dropdown">
   <button onclick="myFunction()" class="dropbtn"></button>
   <div id="myDropdown" class="dropdown-content">
@@ -13,4 +13,6 @@
     <a href="/logout.php">Sign out</a>
   </div >
 </div>
+
+<?php endif ;?>
 </div>
